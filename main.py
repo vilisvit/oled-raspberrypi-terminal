2ca659dc84ab115062584e18b69d08f958a6dbfe
vilisvit <duck.vilys@gmail.com>
2024-02-25 08:37:13 +0000

    first stable version

---
+++ main.py
@@ -0,0 +1,131 @@
+import pty
+import os
+import shlex
+import select
+import time
+from threading import Thread
+import re
+import pyte
+from pynput import keyboard
+
+import oled
+
+class Terminal:
+    
+    def __init__(self):
+
+        max_read_bytes=1024 * 20
+
+        #spawn shell
+        child_pid, self.fd = pty.fork()
+        if child_pid == 0:  # Child.
+            argv = shlex.split("bash")
+            env = dict(TERM="linux", LC_ALL="en_GB.UTF-8",
+            COLUMNS=str(oled.CHARS_WIDTH), LINES=str(oled.CHARS_HEIGHT))
+            os.execvpe(argv[0], argv, env)
+            
+        #create pyte object
+        self.screen = pyte.HistoryScreen(oled.CHARS_WIDTH, oled.CHARS_HEIGHT, ratio = 1/oled.CHARS_HEIGHT)
+        self.stream = pyte.Stream(self.screen)
+
+
+    def listen_keyboard(self):
+        
+        currently_pressed = set()
+        
+        spec_keys = {
+            'enter': '\r',
+            'esc': '\x1b',
+            'home': '\x1b[H',
+            'end': '\x1b[F',
+            'backspace': '\x7f',
+            'delete': '\x1b[3~',
+            'space': ' ',
+            'left': '\u001b[D',
+            'right': '\u001b[C',
+            'up': '\u001b[A',
+            'down': '\u001b[B'
+        }
+        
+        def on_press(keycode):
+            try:
+                key = keycode.char
+            except AttributeError:
+                key = keycode.name
+            currently_pressed.add(key)
+            
+            if not key:
+                return
+            
+            elif key == 'page_down':
+                screen.next_page()
+                oled.update_screen(self.screen.display, self.screen.cursor.x, self.screen.cursor.y)
+            elif key == 'page_up':
+                screen.prev_page()
+                oled.update_screen(self.screen.display, self.screen.cursor.x, self.screen.cursor.y)
+
+            elif len(key) == 1:
+                
+                is_pressed_non_single_len_key = False
+                if 'shift' in currently_pressed:
+                    currently_pressed.remove('shift')
+                if 'shift_r' in currently_pressed:
+                    currently_pressed.remove('shift_r')
+                if 'ctrl' in currently_pressed:
+                    os.write(fd, chr(ord(key)-96).encode())
+                    
+                for k in currently_pressed:
+                    if len(k) > 1:
+                        is_pressed_non_single_len_key = True
+
+                #not allow input while pressed non-single-len special keys (except shift)
+                if not is_pressed_non_single_len_key:
+                    os.write(self.fd, key.encode())
+                else:
+                    return
+                    
+            elif key in spec_keys:
+                os.write(self.fd, spec_keys[key].encode())        
+                
+        def on_release(keycode):
+            try:
+                key = keycode.char
+            except AttributeError:
+                key = keycode.name
+            if key in currently_pressed:
+                currently_pressed.remove(key)
+        
+        while True:
+            with keyboard.Listener(on_press=on_press, on_release=on_release, suppress=True) as listener:
+                listener.join()
+        return input_text
+
+    def read_and_forward_pty_output(self):
+        max_read_bytes = 1024 * 20
+        global lines
+        iteration = 0
+        while True:
+            if self.fd:
+                timeout_sec = 0
+                (data_ready, _, _) = select.select([self.fd], [], [], timeout_sec)
+                if data_ready:
+                    output = os.read(self.fd, max_read_bytes).decode(errors="ignore")
+                    
+                    self.stream.feed(output)
+                    
+                    oled.update_screen(self.screen.display, self.screen.cursor.x, self.screen.cursor.y)
+
+    def run_shell(self):
+        global lines
+
+        # Define the commands
+        command = ""
+
+        output_thread = Thread(target=self.read_and_forward_pty_output)
+        output_thread.start()
+
+        while True:
+            self.listen_keyboard()
+
+terminal = Terminal()
+terminal.run_shell()
---
+++ oled.py
@@ -0,0 +1,60 @@
+import time
+import board
+import digitalio
+
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# Display Parameters
+OLED_WIDTH = 128
+OLED_HEIGHT = 64
+
+# Use for I2C.
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+# Clear display.
+oled.fill(0)
+oled.show()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new('1', (OLED_WIDTH, OLED_HEIGHT))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, OLED_WIDTH,OLED_HEIGHT), outline=0, fill=0)
+
+FONT_SIZE = 6
+
+font = ImageFont.truetype('tiny.ttf', FONT_SIZE)
+
+CHARS_HEIGHT = 10
+CHARS_WIDTH = 32
+
+
+def update_screen(lines, cursor_x, cursor_y):
+    draw.rectangle((0, 0, OLED_WIDTH, OLED_HEIGHT), outline=0, fill=0)
+    for line_num in range(len(lines)):
+        line = lines[line_num]
+        if cursor_y == line_num:
+            before_cursor = line[:cursor_x]
+            cursor_char = line[cursor_x]
+            after_cursor = line[cursor_x + 1:]
+            draw.text((0, line_num * (FONT_SIZE)), before_cursor, font=font, fill=255)
+            (left, top, right, bottom) = draw.textbbox((font.getsize(before_cursor)[0], line_num * (FONT_SIZE)), cursor_char, font=font)
+            draw.rectangle((left, top, right-1, bottom), fill=255)
+            draw.text((font.getsize(before_cursor)[0], line_num * (FONT_SIZE)), cursor_char, font=font, fill=0)
+            draw.text((font.getsize(before_cursor)[0] + font.getsize(cursor_char)[0], line_num * (FONT_SIZE)), after_cursor, font=font, fill=255)
+        else:
+            draw.text((0, line_num * (FONT_SIZE)), line, font=font, fill=255)
+
+    oled.image(image)
+    oled.show()
+    
+
